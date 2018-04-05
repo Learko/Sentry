5c25fd9b6fbd59f625985987783cec1fafcaed38
Learko <matveevantonekb@gmail.com>
2018-04-05 18:34:51 +0500

    Fix sending

--- sentry.py
+++ sentry.py
@@ -1,18 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 
 import sys
 from serial import Serial
 from time import sleep
 
 
+hello = b'''\r\nGrbl 1.1f ['$' for help]\r\n'''
+ok = b'''ok\r\nok\r\n'''
+
+
 class Sentry(object):
     def __init__(self, tty, baudrate):
         self._serial = Serial(tty, baudrate)
 
-        self.x_rate = 1000
-        self.y_rate = 1000
-        self.x_acc  = 40
-        self.y_acc  = 40
+        b = b''
+        while b != hello:
+            b += self._serial.read_all()
+
+        self.x_rate = 5000
+        self.y_rate = 4000
+        self.x_acc  = 1000
+        self.y_acc  = 1000
 
         self._feed_rate = 9000
 
@@ -26,21 +34,13 @@ class Sentry(object):
 
 
     def send(self, cmd):
-        while not self._serial.in_waiting:
-            print('1'*80)
-
         self._serial.write(f'{cmd}\r\n'.encode('ascii'))
 
-        while not self._serial.in_waiting:
-            print('2'*80)
 
-        self._serial.write(f'$$\r\n'.encode('ascii'))
+        code = b''
+        while code != ok:
+            code += self._serial.read_all()
 
-        while not self._serial.in_waiting:
-            print('3'*80)
-
-        for l in self._serial.read_all().decode().split('\r\n'):
-            print(l)
 
     def recv(self):
         return self._serial.read_all()
@@ -55,8 +55,6 @@ class Sentry(object):
         self._x_rate = rate
         self.send(f'$110={rate}')
 
-        print(f'Setting x_rate: {rate}')
-
 
     @property
     def y_rate(self):
@@ -66,8 +64,6 @@ class Sentry(object):
     def y_rate(self, rate):
         self._y_rate = rate
         self.send(f'$111={rate}')
-
-        print(f'Setting y_rate: {rate}')
 
 
     @property
@@ -79,8 +75,6 @@ class Sentry(object):
         self._x_acc = acc
         self.send(f'$120={acc}')
 
-        print(f'Setting x_acc: {acc}')
-
 
     @property
     def y_acc(self):
@@ -91,8 +85,6 @@ class Sentry(object):
         self._y_acc = acc
         self.send(f'$121={acc}')
 
-        print(f'Setting y_acc: {acc}')
-
 
 
 
@@ -101,6 +93,11 @@ if __name__ == '__main__':
         sys.exit('Python 3.6 or later is required.\n')
 
     sentry = Sentry('/dev/ttyACM0', 115200)
-    sentry.jog(50, 0)
-    sentry.jog(-50, 0)
+
+    for _ in range(5):
+        for _ in range(50):
+            sentry.jog(1, 1)
+
+        for _ in range(50):
+            sentry.jog(-1, -1)
 
