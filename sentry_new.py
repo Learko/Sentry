6073f63b6d4b66055998c95a920598cca8c64110
Learko <matveevantonekb@gmail.com>
2018-05-30 18:11:43 +0500

    Face detection

--- sentry_new.py
+++ sentry_new.py
@@ -4,6 +4,8 @@ import sys
 from serial import Serial
 from time import sleep
 from math import tan, atan, radians, degrees
+from threading import Thread, Lock
+from copy import deepcopy
 
 import cv2
 
@@ -20,11 +22,10 @@ class Sentry(object):
         while b != hello:
             b += self._serial.read_all()
 
-        self.code = ok
         self.x_rate = 5000
         self.y_rate = 4000
-        self.x_acc  = 1000
-        self.y_acc  = 1000
+        self.x_acc  = 100
+        self.y_acc  = 100
 
         self._feed_rate = 9000
 
@@ -34,15 +35,16 @@ class Sentry(object):
 
 
     def jog(self, x, y):
+        # self.reset()
         self.send(f'$J=G91 G21 X{x} Y{y} F{self._feed_rate}')
 
 
     def send(self, cmd):
         self._serial.write(f'{cmd}\r\n'.encode('ascii'))
 
-        self.code = b''
-        while self.code != ok:
-            self.code += self._serial.read_all()
+        code = b''
+        while code != ok:
+            code += self._serial.read_all()
 
 
     def recv(self):
@@ -89,13 +91,67 @@ class Sentry(object):
         self.send(f'$121={acc}')
 
 
+    def idle(self):
+        code = b''
+
+        while not code.startswith(b'<Idle'):
+            self._serial.write(b'?\r\n')
+            code = b''
+
+            while not code.endswith(b'ok\r\n'*2):
+                code += self._serial.read_all()
+
+    def reset(self):
+        self._serial.write(f'{chr(0x18)}'.encode('ascii'))
+
+        code = b''
+        while code != hello:
+            code += self._serial.read_all()
+
+            if code:
+                print(code)
+
+
+
+
+class Camera(object):
+    def __init__(self, id=1):
+        self.id = id
+        self.video_capture = cv2.VideoCapture(self.id)
+        self.size = self.video_capture.get(cv2.CAP_PROP_FRAME_WIDTH), self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        self._frame = None
+        # self._lock = Lock()
+        self._stop = False
+
+        self.update_thread = Thread(target=self._update, args=(), daemon=True)
+        self.update_thread.start()
+
+    def __del__(self):
+        self._stop = True
+
+
+    def _update(self):
+        while not self._stop:
+            # with self._lock:
+            ret, self._frame = self.video_capture.read()
+
+
+    @property
+    def frame(self):
+        # with self._lock:
+        frame = deepcopy(self._frame)
+
+        return frame
+
+
 
 
 if __name__ == '__main__':
     if sys.version_info < (3, 6):
         sys.exit('Python 3.6 or later is required.\n')
 
-    # sentry = Sentry('/dev/ttyACM0', 115200)
+    sentry = Sentry('/dev/ttyACM0', 115200)
 
     face_casc_path = 'haarcascades/haarcascade_frontalface_default.xml'
     eye_casc_path = 'haarcascades/haarcascade_eye.xml'
@@ -108,13 +164,14 @@ if __name__ == '__main__':
     left_eye_casc = cv2.CascadeClassifier(left_eye_casc_path)
     right_eye_casc = cv2.CascadeClassifier(right_eye_casc_path)
 
-    video_capture = cv2.VideoCapture(0)
+    cam = Camera()
+    cw, ch = cam.size
 
-    cw, ch = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH), video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    print(f'Resolution: {cw}x{ch}')
+    while cam.frame is None:
+        pass
 
     while True:
-        ret, frame = video_capture.read()
+        frame = cam.frame
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -158,20 +215,19 @@ if __name__ == '__main__':
             shift_x = cw/2 - x
             shift_y = ch/2 - y
 
-            k = 1.9
-
             jx = 0
             jy = 0
 
-            if abs(shift_x) > 20:
-                jx = round(degrees(atan(2 * shift_x * tan(radians(65))/cw)) / 30, 4)
-            if abs(shift_y) > 22:
-                jy = round(degrees(atan(2 * shift_y * tan(radians(65))/ch)) / 40, 4)
+            if abs(shift_x) > 10:
+                jx = round(degrees(atan(2 * shift_x * tan(radians(65))/cw)) / 3.5, 4)
+            if abs(shift_y) > 10:
+                jy = round(degrees(atan(2 * shift_y * tan(radians(65))/ch)) / 4, 4)
 
 
             if jx != 0 or jy != 0:
-                print(jx, jy)
-                # sentry.jog(*map(int, (jx*k, jy*k)))
+                # print(jx, jy)
+                sentry.jog(*map(int, (jx, jy)))
+                sentry.idle()
 
 
         key = cv2.waitKey(1) & 0xFF
